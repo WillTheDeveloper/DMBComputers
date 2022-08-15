028ae16cafc5f5c13ef5be134dd88a19ab0e0fac
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-15 14:36:43 +0100

    Installed predis and setup redis for cache and session

--- config/database.php
+++ config/database.php
@@ -121,11 +121,22 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+
+        'clusters' => [
+            'redis' => [
+                [
+                    'host' => env('REDIS_HOST', 'localhost'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
         ],
 
         'default' => [
